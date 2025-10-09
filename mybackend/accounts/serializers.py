45824503c4fbca481e2cd560968439b62425a6c2
Manjunath072004 <manjunatha.l@y4elabs.com>
2025-10-09 14:14:26 +0530

    Day 3 Progress

---
+++ mybackend/accounts/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth.password_validation import validate_password
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'phone_number', 'password')
+    
+    def create(self, validated_data):
+        user = CustomUser.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            phone_number=validated_data['phone_number']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
